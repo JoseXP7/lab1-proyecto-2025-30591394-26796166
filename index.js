5dff9a35fe9dd573c4040387e5756be0ff2f3ad7
JoseXP7 <josemanuelgraterolrodriguez@gmail.com>
2025-11-10 12:05:39 -0400

    feat: Port configuration

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import app from './src/app.js'
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+app.listen(app.get('port'), () => {
+  console.log(`Server is running on port ${app.get('port')}`)
 })
